91be85a31b1a8b4be731b3ba218b1eb8d59b1d9e
Matthew Wallace <github@matthewwallace.me>
2023-01-13 08:12:41 -0700

    Upgrade from the Dark Sky API to pirate weather

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Weather for i3blocks
 
-A simple python script which uses the [Dark Sky API](https://darksky.net/) to
+A simple python script which uses the [Pirate Weather API](https://pirateweather.net/) to
 get location based weather information to display in [i3blocks](https://github.com/vivien/i3blocks).
 
-You will need a Dark Sky API key, which is free and can be obtained easily from
-the [Dark Sky Developer](https://darksky.net/dev/) site. API requests are
-limited to 1000 per day per key, so running the command every 100 seconds will
-keep you under that limit presuming it will be running 24/7.
+You will need a Pirate Weather API key, which is free and can be obtained
+easily from the Pirate Weather site. API requests are limited to 20,000 calls
+per month per key, so running the command every 15 minutes will keep you under
+that limit presuming it will be running 24/7.
 
 If a location is not provided via the command line, the script will
 automatically locate you using your IP address. This obviously doesn't work if
@@ -23,7 +23,7 @@ font package and [python-forecastio](https://github.com/ZeevG/python-forecast.io
 Add the following to your `i3blocks.conf` file:
 ```
 [Weather]
-command=/path/to/i3blocks-weather.py -f -k <dark_sky_api_key>
+command=/path/to/i3blocks-weather.py -f -k <pirate_weather_api_key>
 markup=pango
 interval=100
 ```
--- i3blocks-weather.py
+++ i3blocks-weather.py
@@ -126,6 +126,42 @@ def get_icon_hex(options, icon_str):
         icon_hex = 'f07b' # N/A
     return (degrees_hex, icon_hex)
 
+# Hacked up URL builder copied from forecastio python code
+def load_forecast(key, lat, lng, time=None, units="auto", lang="en", lazy=False,
+                  callback=None):
+    """
+        This function builds the request url and loads some or all of the
+        needed json depending on lazy is True
+        inLat:  The latitude of the forecast
+        inLong: The longitude of the forecast
+        time:   A datetime.datetime object representing the desired time of
+               the forecast. If no timezone is present, the API assumes local
+               time at the provided latitude and longitude.
+        units:  A string of the preferred units of measurement, "auto" id
+                default. also us,ca,uk,si is available
+        lang:   Return summary properties in the desired language
+        lazy:   Defaults to false.  The function will only request the json
+                data as it is needed. Results in more requests, but
+                probably a faster response time (I haven't checked)
+    """
+
+    if time is None:
+        url = 'https://dev.pirateweather.net/forecast/%s/%s,%s' \
+              '?units=%s&lang=%s' % (key, lat, lng, units, lang)
+    else:
+        url_time = time.replace(microsecond=0).isoformat()  # API returns 400 for microseconds
+        url = 'https://dev.pirateweather.net/forecast/%s/%s,%s,%s' \
+              '?units=%s&lang=%s' % (key, lat, lng, url_time, units, lang)
+
+    if lazy is True:
+        baseURL = "%s&exclude=%s" % (url,
+                                     'minutely,currently,hourly,'
+                                     'daily,alerts,flags')
+    else:
+        baseURL = url
+
+    return forecastio.manual(baseURL, callback=callback)
+
 def main ():
     # Parse command line arguments
     options = get_options()
@@ -136,8 +172,8 @@ def main ():
     else:
         (lat, lon, location) = get_ip_location()
 
-    # Load the forecast from Dark Sky (aka forecastio)
-    forecast = forecastio.load_forecast(options.api_key, lat, lon, units='us')
+    # Load the forecast from Pirate Weather
+    forecast = load_forecast(options.api_key, lat, lon, units='us')
     (temp, icon_str) = get_current_forecast(options, forecast)
 
     # If the weather icon is pressed, this environment variable will be set.
